496cb2272969e0f657b8ae1249aa770c7adbf2e7
Md-Jarif-Hosain <hosainjarif@gmail.com>
2023-03-04 23:52:29 +0600

    Full website created

---
+++ JS/app.js
@@ -0,0 +1,115 @@
+/** @format */
+/* ---------------------------- Pre-loader --------------------- */
+window.addEventListener("load", () => {
+  document.querySelector(".main").classList.remove("hidden");
+  document.querySelector(".home-section").classList.add("active");
+  document.querySelector(".page-loader").classList.add("fade-out");
+
+  setTimeout(() => {
+    document.querySelector(".page-loader").style.display = "none";
+  }, 600);
+});
+
+/* ---------------------------- Typed Js --------------------- */
+var typed = new Typed(".typed", {
+  strings: ["frontend web developer.", "Programmer."],
+  typeSpeed: 60,
+  backSpeed: 60,
+  loop: true,
+});
+
+/* ---------------------------- Toggle navbar--------------------- */
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", () => {
+  hideSection();
+  toggleNavbar();
+  document.body.classList.toggle("hide-scrolling");
+});
+
+let hideSection = () => {
+  document.querySelector("section.active").classList.toggle("fade-out");
+};
+
+let toggleNavbar = () => {
+  document.querySelector(".header").classList.toggle("active");
+};
+
+/* ------------ Active section --------------------------------*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("link-item") && e.target.hash !== "") {
+    document.querySelector(".overlay").classList.add("active");
+    navToggler.classList.add("hide");
+    if (e.target.classList.contains("nav-item")) {
+      toggleNavbar();
+    } else {
+      hideSection();
+      document.body.classList.add(".hide-scrolling");
+    }
+    setTimeout(() => {
+      document
+        .querySelector("section.active")
+        .classList.remove("active", "fade-out");
+      document.querySelector(e.target.hash).classList.add("active");
+      window.scrollTo(0, 0);
+      document.body.classList.remove(".hide-scrolling");
+      navToggler.classList.remove("hide");
+      document.querySelector(".overlay").classList.remove("active");
+    }, 500);
+  }
+});
+
+// ----------------About tabs
+const tabsContainer = document.querySelector("   .about-tabs"),
+  aboutSection = document.querySelector(".about-section");
+
+tabsContainer.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("tab-item") &&
+    !e.target.classList.contains("active")
+  ) {
+    tabsContainer.querySelector(".active").classList.remove("active");
+    e.target.classList.add("active");
+    const target = e.target.getAttribute("data-target");
+    aboutSection
+      .querySelector(".tab-content.active")
+      .classList.remove("active");
+    aboutSection.querySelector(target).classList.add("active");
+  }
+});
+
+/* -------------------------- Portfolio item details popup*/
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("view-project-btn")) {
+    togglePortfolioPopup();
+    document.querySelector(".portfolio-popup").scrollTo(0, 0);
+    portfolioItemDetails(e.target.parentElement);
+  }
+});
+let togglePortfolioPopup = () => {
+  document.querySelector(".portfolio-popup").classList.toggle("open");
+  document.body.classList.toggle("hide-scrolling");
+  document.querySelector(".main").classList.toggle("fade-out");
+};
+let portfolioItemDetails = (portfolioItem) => {
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(
+    ".portfolio-item-thumbnail img"
+  ).src;
+
+  document.querySelector(".pp-header h3").innerHTML =
+    portfolioItem.querySelector(".portfolio-item-title").innerHTML;
+
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(
+    ".portfolio-item-details"
+  ).innerHTML;
+};
+
+document
+  .querySelector(".pp-close")
+  .addEventListener("click", togglePortfolioPopup);
+
+/* --------------------- hide popup when clicking outside of it ---------- */
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("pp-inner")) {
+    togglePortfolioPopup();
+  }
+});
